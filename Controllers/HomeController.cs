c6ca791b315c588bf0a9205c2f5ea9e7802ce97a
JONATAN NAIFELD <49549062@ort>
2025-06-05 14:33:37 -0300

    controllers empezados

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,10 +14,59 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+// cuando empieza el juego, guardar en session el COTO inicializado, y cada vez q se emtre a una sala pedirlo del session para tener la info de la sala
+// cada vez q se gana la sala se pide y se actualiza la variable de salas completadas
     public IActionResult Index()
     {
         
         return View();
     }
 
+    public IActionResult Almacenamiento()
+    {
+        
+        return View();
+    }
+
+    public IActionResult Mantenimiento()
+    {
+        
+        return View();
+    }
+
+    public IActionResult Termica()
+    {
+        
+        return View();
+    }
+
+    public IActionResult Heladeras()
+    {
+        
+        return View();
+    }
+
+    public IActionResult Verduleria()
+    {
+        
+        return View();
+    }
+
+    public IActionResult Registradora()
+    {
+        
+        return View();
+    }
+
+    public IActionResult CajaAbierta()
+    {
+        
+        return View();
+    }
+
+    public IActionResult Escape()
+    {
+        
+        return View();
+    }
 }
